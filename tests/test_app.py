5e70fa1d896f5826c27e2551e215da9d9e7de260
Tutusr32 <rezendea142@gmail.com>
2026-03-02 21:29:51 -0300

    feat: Apresentação de FastAPI

---
+++ tests/test_app.py
@@ -0,0 +1,24 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from fast_zero.app import app
+
+
+def test_root_deve_retornar_ola_mundo():
+    """
+    Esse teste tem 3 etapas (AAA)
+    - A: Arrange - Arranjo
+    - A: Act     - Executa a ação
+    - A: Assert  - Garantia da igualidade
+    """
+    # Arrange
+    client = TestClient(app)
+
+    # Act
+    response = client.get('/')
+
+    # Assert
+    assert response.json() == {'message': 'Olá Mundo!'}
+    assert response.status_code == HTTPStatus.OK
+     
